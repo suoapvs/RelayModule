1e2487916c2dc8111f2d3705c99a25fe7e2cae0d
Yurii Salimov <yuriy.alex.salimov@gmail.com>
2018-02-05 00:19:42 +0200

    Added an example.

---
+++ examples/RelayBlink/RelayBlink.ino
@@ -0,0 +1,33 @@
+/*
+  Relay Blink
+
+  Turns an Relay on for one second,
+  then off for one second, repeatedly.
+
+  https://github.com/YuriiSalimov/RelayModule
+  
+  Created by Yurii Salimov, February, 2018.
+  Released into the public domain.
+*/
+
+#include <RelayModule.h>
+
+#define RELAY_PIN   1
+#define DELAY_TIME  1000
+
+RelayModule* relay;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  relay = new RelayModule(RELAY_PIN);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  if (relay->isOn()) {
+    relay->off(); // turn the relay off.
+  } else if (relay->isOff()) {
+    relay->on(); // turn the relay on.
+  }
+  delay(DELAY_TIME); // wait for a second.
+}
