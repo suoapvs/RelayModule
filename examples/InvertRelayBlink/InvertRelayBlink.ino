343d8c7d2e8092c426f29a802472cff26148f65d
Yurii Salimov <yuriy.alex.salimov@gmail.com>
2018-04-15 22:16:16 +0300

    Added a new example.

---
+++ examples/InvertRelayBlink/InvertRelayBlink.ino
@@ -0,0 +1,39 @@
+/*
+  Relay Blink (Inverted)
+
+  Turns an Relay on for one second,
+  then off for one second, repeatedly.
+
+  https://github.com/YuriiSalimov/RelayModule
+
+  Created by Yurii Salimov, February, 2018.
+  Released into the public domain.
+*/
+
+#include <RelayModule.h>
+
+#define RELAY_PIN   1
+#define DELAY_TIME  1000
+#define INVERT_RELAY true
+
+RelayModule* relay;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  relay = new RelayModule(RELAY_PIN, INVERT_RELAY);
+  /** or
+     relay = new RelayModule(RELAY_PIN);
+     relay->invert();
+  */
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  if (relay->isOn()) {
+    relay->off(); // turn the relay off.
+  } else if (relay->isOff()) {
+    relay->on(); // turn the relay on.
+  }
+  delay(DELAY_TIME); // wait for a delay time.
+}
+
